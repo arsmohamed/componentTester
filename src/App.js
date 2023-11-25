efa48dc72eb8323f18cc9bd80b936ac5ba318bbd
arsmohamed <arsmohamed@outlook.com>
2023-11-25 19:07:56 -0330

    text editing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from 'react';
+import TextEditor from './TextEditor';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [componentCode, setComponentCode] = useState('');
+
+  const handleCodeChange = (code) => {
+    setComponentCode(code);
+  };
+
   return (
     <div className="App">
-        Testing
+         <h1>Component Testing Interface</h1>
+      <TextEditor onCodeChange={handleCodeChange} />
     </div>
   );
 }
